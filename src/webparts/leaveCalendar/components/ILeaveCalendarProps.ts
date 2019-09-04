a624f6cf0cb20eaae15b8dae89f1d965a07ea557
Maxim Logunov <m.logunov@outlook.com>
2019-09-04 12:17:31 +0300

    Create a new project

---
+++ src/webparts/leaveCalendar/components/ILeaveCalendarProps.ts
@@ -0,0 +1,3 @@
+export interface ILeaveCalendarProps {
+  description: string;
+}
---
+++ src/webparts/leaveCalendar/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ILeaveCalendarWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'LeaveCalendarWebPartStrings' {
+  const strings: ILeaveCalendarWebPartStrings;
+  export = strings;
+}
